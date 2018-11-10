80209bdd7d92d4313be3ff76b7e796686583ec76
Cameron <41878720+Camo32567@users.noreply.github.com>
2018-11-10 23:30:29 +1300

    Updated

--- Ball.js
+++ Ball.js
@@ -24,28 +24,26 @@ class Ball { //Class for the ball that holds the draw, move and collison Functio
         if (this.x > bouncer.x && this.x < bouncer.x + bouncer.w &&
             this.y + this.h > bouncer.y && this.y < bouncer.y + bouncer.h) {
             this.ySpeed = this.ySpeed * -1;
-            
-            if(this.x < bouncer.x + bouncer.w * 0.25){ // left edge paddle
-                if(this.xSpeed < 0){
-                        this.xSpeed *= -1;
-                        this.xSpeed *= 2;
-                   }else {
-                        this.xSpeed *= 2;
-                   }
-            }
-            if(this.x > bouncer.x + bouncer.w * 0.75){ //when hitting right side of paddle
-                if(this.xSpeed > 0){
-                        this.xSpeed *= -1;
-                        this.xSpeed *= 1.5;
-                   }else {
-                        this.xSpeed *= 1.5;
-                   }
-            }
-            else{ // bounce in middle set back to normal
-                if(this.xSpeed > 0) {
-                    this.xSpeed = 3;
+
+            if (this.x < bouncer.x + bouncer.w * 0.25) { // left edge paddle makes the ball speed *2
+                if (this.xSpeed < 0) {
+                    this.xSpeed *= -1;
+                    this.xSpeed *= 2;
+                } else { //determinds which direction it coming from and to send it which way by using the if and else statment to make - or +
+                    this.xSpeed *= 2;
                 }
-                else {
+            }
+            if (this.x > bouncer.x + bouncer.w * 0.75) { //when hitting right side of paddle it makeks it faster but not as fast as right side and slower than normal
+                if (this.xSpeed > 0) {
+                    this.xSpeed *= -1;
+                    this.xSpeed *= 1.5;
+                } else { //determinds which direction it coming from and to send it which way by using the if and else statment to make - or +
+                    this.xSpeed *= 1.5;
+                }
+            } else { // bounce in middle set back to normal speed
+                if (this.xSpeed > 0) {
+                    this.xSpeed = 3;
+                } else { //determinds which direction it coming from and to send it which way by using the if and else statment to make - or +
                     this.xSpeed = -3;
                 }
             }
@@ -56,22 +54,23 @@ class Ball { //Class for the ball that holds the draw, move and collison Functio
         this.x = this.x + this.xSpeed;
         this.y = this.y + this.ySpeed;
 
-        if (this.x < 0) {
+        if (this.x < 0) { //changes direction if touching left
             this.xSpeed = this.xSpeed * -1;
         }
-        if (this.x + this.w > canvas.width) {
+        if (this.x + this.w > canvas.width) { //changes the ball direction if touching the right
             this.xSpeed = this.xSpeed * -1;
         }
         if (this.y < 0) {
-            this.ySpeed = this.ySpeed * -1;
+            this.ySpeed = this.ySpeed * -1; //changes the ball direction if touchingtop  wall
         }
-        if (this.y > canvas.height - BALL_HEIGHT) {
+        if (this.y > canvas.height - BALL_HEIGHT) { //stops ball if touching bottom
             this.ySpeed = 0;
             this.xSpeed = 0;
         }
     }
 
-    paddleHit(item) {
+    //Brick collsions
+    paddleHit(item) { // detects if the brick is hit
         return (this.x <= (item.x + item.w) &&
                 (this.x + this.w) >= item.x) &&
             (this.y <= (item.y + item.h) &&
@@ -80,25 +79,25 @@ class Ball { //Class for the ball that holds the draw, move and collison Functio
     haspaddleHit(paddle) {
         return this.paddleHit(paddle);
     }
-    hasCollided() {
-        var self = this;
-        var collided = false;
+    hasCollided() { // fucntion for when ball collides with bricks
+        var self = this; //a variable for this. called self
+        var collided = false; //collided var
 
         Bricks.forEach(function (paddle, i) {
             if (self.haspaddleHit(paddle)) {
 
-                if (self.y < paddle.y && self.x + self.w > paddle.x) {
+                if (self.y < paddle.y && self.x + self.w > paddle.x) { // brick top bounce
                     self.ySpeed *= -1;
                     self.xSpeed *= 1;
                     delete Bricks[i];
                     score++
-                } // brick top bounce
-                 if (self.y + self.h > paddle.y + paddle.h && self.x < paddle.x + paddle.w) {
+                }
+                if (self.y + self.h > paddle.y + paddle.h && self.x < paddle.x + paddle.w) { // bottom brick bounce
                     self.ySpeed *= -1;
                     self.xSpeed *= 1;
                     delete Bricks[i];
                     score++
-                } // bottom brick bounce
+                }
                 if (self.x < paddle.x && self.y < paddle.y + paddle.h) { // left edge bounce off brick horizontal move
                     self.xSpeed *= -1;
                     delete Bricks[i];
--- Brick.js
+++ Brick.js
@@ -7,7 +7,7 @@ class Brick { //Brick class
         this.c = c;
     }
 
-    drawBrick() {
+    drawBrick() { //draws brick
         canvasContext.fillStyle = this.c;
         canvasContext.fillRect(this.x, this.y, this.w, this.h);
     }
@@ -18,22 +18,22 @@ var brickRows = 0;
 function makeBricks() { // Function that makes the bricks and pusheds them into an array
     var gap = 10;
     var brickCol = brickCount % 13; //13 bricks per row to fill up screen
-    
-    if(brickCount % 13 == 0 && brickCount > 0){
-       brickRows++; // once there are 13 bricks in a row it starts to make bricks under it
+
+    if (brickCount % 13 == 0 && brickCount > 0) {
+        brickRows++; // once there are 13 bricks in a row it starts to make bricks under it using mod %
     }
-    
+
     const BRICK_WIDTH = 50;
     const BRICK_HEIGHT = 25;
     var xPos = brickCol * (BRICK_WIDTH + gap) + gap;
-    var yPos = brickRows * (BRICK_HEIGHT + gap) + gap;
-    
+    var yPos = brickRows * (BRICK_HEIGHT + gap) + gap; //varibables for bricks and the postions of each one 
+
     console.log(xPos + ':' + yPos);
-    
+
     var color = 'white';
 
     var b = new Brick(xPos, yPos, BRICK_WIDTH, BRICK_HEIGHT, color);
-    Bricks.push(b);
-    
-    brickCount++;
+    Bricks.push(b); //pushed the brick into an array 
+
+    brickCount++; //adds to the brick count so knows when to start new row
 }
